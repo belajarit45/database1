e905eb77891be26245db3f7f32648131566e95ce
belajarit45 <144545476+belajarit45@users.noreply.github.com>
2024-07-05 16:02:31 +0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,3 +1,5 @@
+version: '3'
+
 services:
   earnapp1:
     container_name: earnapp1
@@ -5,6 +7,8 @@ services:
     restart: always
     environment:
       - EARNAPP_UUID=sdk-node-a4440cfc329f49ecbc349e43f90c9c25
+    volumes:
+      - earnapp-data:/etc/earnapp
 
   earnapp2:
     container_name: earnapp2
@@ -12,6 +16,8 @@ services:
     restart: always
     environment:
       - EARNAPP_UUID=sdk-node-a5d04caa43a64867bc008c0f533e33b6
+    volumes:
+      - earnapp-data:/etc/earnapp
 
   earnapp3:
     container_name: earnapp3
@@ -19,6 +25,8 @@ services:
     restart: always
     environment:
       - EARNAPP_UUID=sdk-node-693434f335914a07872ca17b554c5748
+    volumes:
+      - earnapp-data:/etc/earnapp
 
   earnapp4:
     container_name: earnapp4
@@ -26,6 +34,8 @@ services:
     restart: always
     environment:
       - EARNAPP_UUID=sdk-node-80b9d5b677a94bd68c3baaa77511cea7
+    volumes:
+      - earnapp-data:/etc/earnapp
 
   earnapp5:
     container_name: earnapp5
@@ -33,3 +43,8 @@ services:
     restart: always
     environment:
       - EARNAPP_UUID=sdk-node-6c6a7d495c0843e5bdc3b869bdaf5a05
+    volumes:
+      - earnapp-data:/etc/earnapp
+
+volumes:
+  earnapp-data:
