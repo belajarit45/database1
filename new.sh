324d72af744c0742f8497f09c9ebb3f8389ce56e
belajarit45 <144545476+belajarit45@users.noreply.github.com>
2024-07-08 08:05:53 +0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -4,7 +4,7 @@ services:
     image: fazalfarhan01/earnapp:lite
     restart: always
     environment:
-      - EARNAPP_UUID=sdk-node-a4440cfc329f49ecbc349e43f90c9c25
+      - EARNAPP_UUID=sdk-android-a4440cfc329f49ecbc349e43f90c9c25
     volumes:
       - earnapp-data:/etc/earnapp
 
@@ -13,7 +13,7 @@ services:
     image: fazalfarhan01/earnapp:lite
     restart: always
     environment:
-      - EARNAPP_UUID=sdk-node-a5d04caa43a64867bc008c0f533e33b6
+      - EARNAPP_UUID=sdk-android-a5d04caa43a64867bc008c0f533e33b6
     volumes:
       - earnapp-data:/etc/earnapp
 
@@ -22,7 +22,7 @@ services:
     image: fazalfarhan01/earnapp:lite
     restart: always
     environment:
-      - EARNAPP_UUID=sdk-node-693434f335914a07872ca17b554c5748
+      - EARNAPP_UUID=sdk-android-693434f335914a07872ca17b554c5748
     volumes:
       - earnapp-data:/etc/earnapp
 
@@ -31,7 +31,7 @@ services:
     image: fazalfarhan01/earnapp:lite
     restart: always
     environment:
-      - EARNAPP_UUID=sdk-node-80b9d5b677a94bd68c3baaa77511cea7
+      - EARNAPP_UUID=sdk-android-80b9d5b677a94bd68c3baaa77511cea7
     volumes:
       - earnapp-data:/etc/earnapp
 
@@ -40,7 +40,7 @@ services:
     image: fazalfarhan01/earnapp:lite
     restart: always
     environment:
-      - EARNAPP_UUID=sdk-node-6c6a7d495c0843e5bdc3b869bdaf5a05
+      - EARNAPP_UUID=sdk-android-6c6a7d495c0843e5bdc3b869bdaf5a05
     volumes:
       - earnapp-data:/etc/earnapp
 
